090f65e370cd0c9cc233d4df46ebc0cd60be91ce
emry24 <em_ryden@outlook.com>
2024-03-08 05:40:58 +0100

    alert message added, signin modified

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,26 +8,29 @@ using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
-public class AuthController : Controller
+public class AuthController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager) : Controller
 {
-    private readonly UserManager<UserEntity> _userManager;
-
-    public AuthController(UserManager<UserEntity> userManager)
-    {
-        _userManager = userManager;
-    }
+    private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly SignInManager<UserEntity> _signInManager = signInManager;
 
     [HttpGet]
     [Route("/signup")]
-    public IActionResult SignUp() => View(new SignUpViewModel());
+    public IActionResult SignUp()
+    {
+        if (_signInManager.IsSignedIn(User))
+        {
+            return RedirectToAction("Details", "Account");
+        }
+        else
+        {
+            var viewModel = new SignUpViewModel
+            {
+                Title = "Sign Up"
+            };
 
-    //[HttpGet]
-    //[Route("/signup")]
-    //public IActionResult SignUp()
-    //{
-    //    var viewModel = new SignUpViewModel();
-    //    return View(viewModel);
-    //}
+            return View(viewModel);
+        }
+    }
 
     [HttpPost]
     [Route("/signup")]
@@ -59,31 +63,52 @@ public class AuthController : Controller
             }
         
             return View(viewModel);
-
     }
-
 
     [HttpGet]
     [Route("/signin")]
     public IActionResult SignIn()
     {
-        var viewModel = new SignInViewModel();
-        return View(viewModel);
+        if (_signInManager.IsSignedIn(User))
+        {
+            return RedirectToAction("Details", "Account");
+        }
+        else
+        {
+            var viewModel = new SignInViewModel
+            {
+                Title = "Sign In"
+            };
+
+            return View(viewModel);
+        }
     }
 
     [HttpPost]
     [Route("/signin")]
-    public IActionResult SignIn(SignInViewModel viewModel)
+    public async Task<IActionResult> SignIn(SignInViewModel viewModel)
     {
-        if (!ModelState.IsValid)
-            return View(viewModel);
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Form.Email, viewModel.Form.Password, viewModel.Form.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Details", "Account");
+            }
+        }
 
-        //var result = _authService.SignIn(viewModel.Form);
-        //if (result)
-        //    return RedirectToAction("Account", "Index");
-
-        viewModel.ErrorMessage = "Incorrect email or password";
+        ModelState.AddModelError("IncorrectValues", "Incorrect email or password");
+        ViewData["ErrorMessage"] = "Incorrect email or password";
         return View(viewModel);
     }
 
+    [HttpGet]
+    [Route("/signout")]
+    public new async Task<IActionResult> SignOut()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
 }
+
