40d69cdc06b66ce99974408b3421c02c8885ef26
emry24 <em_ryden@outlook.com>
2024-04-17 03:34:14 +0200

    contact init

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+using WebApp.ViewModels;
+using Newtonsoft.Json;
+using System.Text;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class ContactController(HttpClient http) : Controller
+{
+    private readonly HttpClient _http = http;
+
+    //[Authorize]
+    public IActionResult Index()
+    {
+        var viewModel = new ContactViewModel();
+
+        return View(viewModel);
+    }
+
+
+    //[HttpPost]
+    //public async Task<IActionResult> ContactForm(ContactViewModel viewModel)
+    //{
+    //    if (ModelState.IsValid)
+    //    {
+    //            try
+    //            {
+    //                var content = new StringContent(JsonConvert.SerializeObject(viewModel), Encoding.UTF8, "application/json");
+    //                var response = await _http.PostAsync("https://localhost:7279/api/subscribers?key=YTc4YTQ3ZjMtYjNiZC00Mzc2LTlmM2MtZGQ3MDg0ZGI0YzNk", content);
+
+    //                if (response.IsSuccessStatusCode)
+    //                {
+    //                    ViewData["Status"] = "Success";
+    //                }
+    //                else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+    //                {
+    //                    ViewData["Status"] = "AlreadyExists";
+    //                }
+    //                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+    //                {
+    //                    ViewData["Status"] = "Unauthorized";
+    //                }
+    //            }
+    //            catch
+    //            {
+    //                ViewData["Status"] = "ConnectionFailed";
+    //            }
+    //    }
+    //    else
+    //    {
+    //        ViewData["Status"] = "Invalid";
+    //    }
+
+    //    return View("Contact", viewModel);
+    //}
+}
---
+++ WebApp/Models/ContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class ContactModel
+{
+    [DataType(DataType.Text)]
+    [Display(Name = "Full name", Prompt = "Enter your full name")]
+    [Required(ErrorMessage = "Full name is required")]
+    [MinLength(2, ErrorMessage = "Full name is required")]
+    public string FullName { get; set; } = null!;
+
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email Address", Prompt = "Enter your email address")]
+    [Required(ErrorMessage = "Enter a valid email address")]
+    [RegularExpression(@"^[^\s@]+@[^\s@]+\.[^\s@]{2,}$", ErrorMessage = "Your email address is invalid")]
+    public string Email { get; set; } = null!;
+
+    public string? Service { get; set; }
+
+    [Display(Name = "Message", Prompt = "Message")]
+    [DataType(DataType.MultilineText)]
+    public string? Message { get; set; } = null!;
+}
